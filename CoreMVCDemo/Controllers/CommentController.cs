dbec2f69301146ef97ab493a58d6b4dba6bb0ec1
cihanbahcivan <cihanbhcvn@gmail.com>
2022-02-07 23:57:50 +0300

    39. Ders Sonu

--- CoreMVCDemo/Controllers/CommentController.cs
+++ CoreMVCDemo/Controllers/CommentController.cs
@@ -5,19 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 
 namespace CoreMVCDemo.Controllers
 {
     public class CommentController : Controller
     {
         private CommentManager commentManager = new CommentManager(new EfCommentRepository());
+
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+
         public PartialViewResult PartialAddComment()
         {
+            return PartialView();
+        }
+
+        [HttpPost]
+
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+            p.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            p.BlogId = 2;
+
+            commentManager.Add(p);
             return PartialView();
         }
 
---
+++ CoreMVCDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace CoreMVCDemo.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        private NewsLetterManager _newsLetterManager = new NewsLetterManager(new EfNewsLetterRepository());
+
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SubscribeMail(NewsLetter p)
+        {
+            p.MailStatus = true;
+            _newsLetterManager.AddNewsLetter(p);
+            return PartialView();
+        }
+    }
+}

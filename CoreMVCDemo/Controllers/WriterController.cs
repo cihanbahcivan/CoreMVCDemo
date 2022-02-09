78867f1b2444944a3f71bf7156f7259e673fa5b9
cihanbahcivan <cihanbhcvn@gmail.com>
2022-02-09 03:34:40 +0300

    51. ders sonu Yazar paneli sayfası

--- CoreMVCDemo/Controllers/WriterController.cs
+++ CoreMVCDemo/Controllers/WriterController.cs
@@ -29,5 +29,17 @@ namespace CoreMVCDemo.Controllers
         {
             return View();
         }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }

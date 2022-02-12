97a3d883a35448c84267e95da154d74fddede6b1
cihanbahcivan <cihanbhcvn@gmail.com>
2022-02-12 15:11:55 +0300

    56. ders sonu Blog ekleme sayfası dropdownList

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Başlık bilgisi boş geçilemez");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog içeriği boş geçilemez");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog görseli boş geçilemez");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("150 karakterden az karakter olmalı");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("5 karakterden fazla karakter olmalı");
+
+        }
+    }
+}
--- CoreMVCDemo/Controllers/BlogController.cs
+++ CoreMVCDemo/Controllers/BlogController.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CoreMVCDemo.Controllers
 {
@@ -29,5 +33,47 @@ namespace CoreMVCDemo.Controllers
             var values = _blogManager.GetBlogListByWriter(1);
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> values = (from x in categoryManager.GetAll()
+                select new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryId.ToString()
+                }).ToList();
+
+            ViewBag.categoryValues = values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog blog)
+        {
+
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult results = blogValidator.Validate(blog);
+            if (results.IsValid)
+            {
+                blog.BlogStatus = true;
+                blog.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                blog.WriterId = 1;
+                _blogManager.Add(blog);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
+
     }
 }

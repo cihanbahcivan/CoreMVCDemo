fb919cd8b669651b9f488b60a1c61d3dd6f5e74d
cihanbahcivan <cihanbhcvn@gmail.com>
2022-02-23 01:15:29 +0300

    63. Ders sonu

---
+++ CoreMVCDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryManager.GetAll();
+            return View(values);
+        }
+
+    }
+}

4e6deb6cf6f979fb05b50cc4c0075a3de35e2ae3
cihanbahcivan <cihanbhcvn@gmail.com>
2022-02-22 15:35:24 +0300

    62. ders sonu BlogListDashboard

---
+++ CoreMVCDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}

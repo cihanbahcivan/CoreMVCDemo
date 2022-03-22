866c34008fae819f880000187f5095539de0c17a
cihanbahcivan <cihanbhcvn@gmail.com>
2022-03-23 02:31:10 +0300

    BlogRating table

--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 
 namespace DataAccessLayer.Repositories
 {
